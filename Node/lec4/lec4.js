0c2b4e618c8b3abdd7c84b5bd0d0d57d7ea13311
Divyansh Patel <pateldivyansh131@gmail.com>
2025-08-13 15:44:41 +0530

    lec4

--- Node/lec4/lec4.js
+++ Node/lec4/lec4.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 3000;
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, 'views'));
 app.get("/", (req, res) => {
     res.send("hello, everyone!");
 });
+app.get("/home", (req, res) => {
+    res.render("index");
+})
 
-// 404 handler - must be last
 app.use((req, res) => {
     res.status(404).send("404 Not Found");
 });
